57ec6e5ebef8091bac8a42e79556242999110517
Junbo Niu <21376334@buaa.edu.cn>
2025-07-01 23:13:17 +0800

    Update train.py

--- llava/train/train.py
+++ llava/train/train.py
@@ -573,7 +573,10 @@ def preprocess_qwen(sources, tokenizer: transformers.PreTrainedTokenizer, has_im
         tokenizer.add_tokens(["<image>"], special_tokens=True)
 
     image_token_index = tokenizer.convert_tokens_to_ids("<image>")
-    im_start, im_end = tokenizer.additional_special_tokens_ids
+    if len(tokenizer.additional_special_tokens_ids)==2:
+        im_start, im_end = tokenizer.additional_special_tokens_ids 
+    else:
+        im_start, im_end=tokenizer.convert_tokens_to_ids("<|im_start|>"),tokenizer.convert_tokens_to_ids("<|im_end|>")
     # unmask_tokens = ["<|im_start|>", "<|im_start|>", "\n"]
     unmask_tokens_idx =  [198, im_start, im_end]
     nl_tokens = tokenizer("\n").input_ids
